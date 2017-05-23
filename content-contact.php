49a28e74c3d746a4a21b5341a04db77a5c77b9dd
Garth Henson <garth@guahanweb.com>
2017-05-23 13:44:38 -0700

    Adding contact form

---
+++ content-contact.php
@@ -0,0 +1,24 @@
+
+<div class="contact-overlay">
+  <div class="w-container"><a class="close-link" href="#">CLOSE&nbsp;&times;</a>
+    <div class="form-wrapper">
+      <div class="contact-heading">Get in Touch</div>
+      <div class="small-divider"></div>
+      <div class="contact-text">Thank you for your interest! Please fill out the form below, and I will do my best to get back to you.</div>
+      <div class="w-form">
+        <form class="email-form" id="email-form" data-name="Email Form">
+          <input class="text-field w-input" id="name" data-name="Name" name="name" maxlength="256" placeholder="Enter your name" required="required"/>
+          <input class="text-field w-input" id="email" data-name="Email" name="email" maxlength="256" placeholder="Enter your email address" required="required"/>
+          <textarea class="text-area text-field w-input" id="project-details" data-name="Project details" name="project-details" maxlength="5000" placeholder="What is your question or project idea?" required="required"></textarea>
+          <input class="submit-button w-button" data-wait="Please wait..." type="submit" value="Send"/>
+        </form>
+        <div class="success-message w-form-done">
+          <p class="success-text">Thank you! Your submission has been received!</p>
+        </div>
+        <div class="w-form-fail">
+          <p>Oops! Something went wrong while submitting the form.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
