d32b0efaea2fb5cfb7f9d43e0cc26a894b5505ff
Garth Henson <garth@guahanweb.com>
2017-05-24 14:00:00 -0700

    This adds the default images to the individual categories and posts

--- post/list-item.php
+++ post/list-item.php
@@ -1,19 +1,27 @@
+<?php
+$categories = get_the_category();
+$cat = $categories[0];
+$term_id = $cat->term_id;
+$term_meta = get_option("taxonomy_$term_id");
+?>
         <div class="blog-block w-col w-col-4 w-dyn-item">
           <a class="blog-link project-link w-inline-block" href="<?php the_permalink(); ?>">
             <div class="blog-image-wrapper">
               <div class="blog-date"><?php the_date(); ?></div>
 <?php
-// let's show the first category as a highlight
-$categories = get_the_category();
-$cat = $categories[0];
-
 if ($cat->slug !== 'uncategorized') {
-  $term_id = $cat->term_id;
-  $term_meta = get_option("taxonomy_$term_id");
   $color = $term_meta['color'];
-
   printf('<div class="blog-category %s" style="background-color: #%s">%s</div>', $cat->slug, $color, $cat->name);
 }
+
+$image = '';
+if (has_post_thumbnail()) {
+  $image = get_the_post_thumbnail_url(null, 'gw-card-thumb');
+} elseif (isset($term_meta['image']) && '' !== $term_meta['image']) {
+  $image_id = $term_meta['image'];
+  $image = wp_get_attachment_image_url($image_id, 'gw-card-thumb');
+}
+printf('<div class="blog-image" style="background-image: url(\'%s\');"></div>', $image);
 ?>
               <div class="blog-image"></div>
             </div>
