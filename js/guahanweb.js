ad8b818ccef4cd7c8a413df7dcd565d2b8f33054
Garth Henson <garth@guahanweb.com>
2017-05-26 12:10:49 -0700

    Error handling for contact form

--- js/guahanweb.js
+++ js/guahanweb.js
@@ -1,15 +1,14 @@
 (function ($) {
     'use strict';
 
-    const $contact_overlay = document.querySelector('div.contact-overlay');
-    const $contact_link = document.querySelectorAll('a.nav-link.contact');
-    const $close_link = document.querySelector('.contact-overlay a.close-link');
+    const $contact_overlay = $('div.contact-overlay');
+    const $close_link = $('.contact-overlay a.close-link');
+    const $success_container = $contact_overlay.find('.w-form-done');
+    const $failure_container = $contact_overlay.find('.w-form-fail');
 
     // Contact form event listeners
-    $close_link.addEventListener('click', hideContactForm);
-    $contact_link.forEach(function (el, i) {
-        el.addEventListener('click', showContactForm);
-    });
+    $close_link.on('click', hideContactForm);
+    $(document).on('click', 'a.nav-link.contact', showContactForm);
 
     const $contact_form = $('form#email-form');
     $contact_form.on('click', 'input[type=submit]', submitContactForm);
@@ -27,6 +26,13 @@
         });
 
         $.post(GW_AJAXURL, data, function (response) {
+            if (!!response.success) {
+                $contact_form.hide();
+                $success_container.show();
+            } else {
+                $contact_form.hide();
+                $failure_container.show();
+            }
             console.log(response);
         });
     }
@@ -34,20 +40,28 @@
     function showContactForm(e) {
         e.preventDefault();
         e.stopPropagation();
-        $contact_overlay.style.display = 'block';
+        $contact_overlay.css('display', 'block');
         setTimeout(function () {
-            $contact_overlay.style.opacity = '1';
-            $contact_overlay.style.transform = 'scaleX(1) scaleY(1) scaleZ(1)';
+            $contact_overlay.css({
+                'opacity': '1',
+                'transform': 'scaleX(1) scaleY(1) scaleZ(1)'
+            });
         }, 100);
     }
 
     function hideContactForm(e) {
         e.preventDefault();
         e.stopPropagation();
-        $contact_overlay.style.opacity = '0';
-        $contact_overlay.style.transform = null;
+        $contact_overlay.css({
+            'opacity': '0',
+            'transform': null
+        });
         setTimeout(function () {
-            $contact_overlay.style.display = 'none';
+            // reset form visibility
+            $contact_overlay.css('display', 'none');
+            $contact_form.show();
+            $success_container.hide();
+            $failure_container.hide();
         }, 600);
     }
 })(jQuery);
