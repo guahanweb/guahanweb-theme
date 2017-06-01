ff84c7ec1ebf1cfc7d9492c59ea2b306340521f2
Garth Henson <garth@guahanweb.com>
2017-06-01 13:58:39 -0700

    Adding twitter card

--- meta-twitter.php
+++ meta-twitter.php
@@ -2,6 +2,6 @@
 <meta name="twitter:site" content="@guahanweb" />
 <?php
 sprintf('<meta name="twitter:title" content="%s" />', single_post_title());
-sprintf('<meta name="twitter:description" content="%s" />', the_summary());
+sprintf('<meta name="twitter:description" content="%s" />', apply_filters('get_the_excerpt', $post->the_excerpt));
 echo '<meta name="twitter:image" content="" />';
 ?>
